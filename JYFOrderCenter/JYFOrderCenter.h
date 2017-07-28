9fc9bf7a21212c007df3621bf491eef8739a04d4
jiangyunfeng <554994782@qq.com>
2017-07-28 10:48:06 +0800

    update

---
+++ JYFOrderCenter/JYFOrderCenter.h
@@ -0,0 +1,19 @@
+//
+//  JYFOrderCenter.h
+//  JYFOrderCenter
+//
+//  Created by jiang on 2017/7/28.
+//  Copyright © 2017年 jiangYunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYFOrderCenter.
+FOUNDATION_EXPORT double JYFOrderCenterVersionNumber;
+
+//! Project version string for JYFOrderCenter.
+FOUNDATION_EXPORT const unsigned char JYFOrderCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYFOrderCenter/PublicHeader.h>
+
+
