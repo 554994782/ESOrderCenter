78d070d6de8116ab80b70acb15c12e25d5e7a609
jiangyunfeng <jiangyunfeng@juran.com.cn>
2017-08-14 13:50:54 +0800

    Update

--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ platform :ios, '8.0'
 inhibit_all_warnings!
 
 source 'https://github.com/554994782/JYFSpecs.git'
+source 'https://github.com/Cocoapods/Specs.git'
 
 target 'JYFOrderCenterDemo’ do
     use_frameworks!
--- Pods/Target Support Files/Pods-JYFOrderCenterDemo/Pods-JYFOrderCenterDemo-frameworks.sh
+++ Pods/Target Support Files/Pods-JYFOrderCenterDemo/Pods-JYFOrderCenterDemo-frameworks.sh
@@ -90,9 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/JYFFoundation/JYFFoundation.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MGJRouter/MGJRouter.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/JYFFoundation/JYFFoundation.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MGJRouter/MGJRouter.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
